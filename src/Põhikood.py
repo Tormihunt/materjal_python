cfefd1a3bca183a26711f4c1ff41f5b366c0e3cf
JProgeja <johannes.palmiste@ut.ee>
2025-12-07 11:58:36 +0200

    commit

--- src/Põhikood.py
+++ src/Põhikood.py
@@ -8,11 +8,9 @@ import csv
 
 def ennusta():
     try:
-        a = list(map(float, sisendid_saama())) #Äkki tuleb mul hiljem parem mõte, kuidas sisendite olemasolu ja kõlblikkust kontrollida
+        test = list(map(float, sisendid_saama())) #Äkki tuleb mul hiljem parem mõte, kuidas sisendite olemasolu ja kõlblikkust kontrollida
     except:
-        hoiatus = Label(m, text="Täida kõik väljad numbritega!")
-        hoiatus.config(bg='red')
-        hoiatus.grid(row=9, column=0)
+        vilgutamine(6)
         return
     csv_loomine()
     regressor = joblib.load("band_gap_rf.joblib")
@@ -21,16 +19,16 @@ def ennusta():
     mean = pd.read_csv("mean_data.csv").iloc[7, 0]
     prediction = regressor.predict(user_var)
     prediction = prediction * standard_deviation + mean
+    prediction = float(prediction[0])
     tulemus = Label(m, text=f'Tulemus: {round(float(prediction), 2)} eV', width=25, height=5)
     tulemus.grid(row = 9, column = 0)
     tulemus.config(bg='lightgreen')
 
 def sisendid_saama():
-    sisendid = [e1.get(), e2.get(), e3.get(), e4.get(), e5.get(), e6.get(), e7.get(), e8.get(), e9.get(), e10.get()]
-    return sisendid
+    return [s.get() for s in sisendid]
 
 def csv_loomine():
-    rida = list(map(int, sisendid_saama()))
+    rida = list(map(float, sisendid_saama()))
     tulbad = ['nelements', 'volume', 'density', 'density_atomic', 'energy_per_atom', 'formation_energy_per_atom', 'energy_above_hull',
               'efermi', 'total_magnetization', 'universal_anisotropy']
     with open("user_variables.csv", "w", newline="") as csvfile:
@@ -39,13 +37,54 @@ def csv_loomine():
         writer.writerow(rida)
     return
 
+def vilgutamine(i):
+    if i == 0:
+        return
+    varv = hoiatus.cget("background")
+    uus = "red" if varv == "orange" else "orange"
+    hoiatus.configure(background=uus)
+    m.after(500, vilgutamine, i-1)
+
+def tuhjenda():
+    for s in sisendid:
+        s.delete(0, END)
+
+def salvesta(f):
+    if f == "":
+        return 0
+    else:
+        with open(f) as vf:
+            i = 0
+            for sisend in sisendid:
+                vf.write(f"{sildid[i]} -- {sisend}")
+                i += 1
+        return True
+
+def salvestusaken():
+    uus = Toplevel(m)
+    uus.title("Salvestamine")
+    uus.geometry("400x300")
+    uus.resizable(False, False)
+    nimi = Label(uus, text="Sisesta faili nimi laiendiga!")
+    uus.columnconfigure(0, weight=1)
+    uus.columnconfigure(1, weight=1)
+    uus.columnconfigure(2, weight=1)
+    nimi.grid(row=0, column=1)
+    nimi.config(font=("Arial", 20))
+    sisend = Entry(uus)
+    sisend.grid(row=1, column=1)
+    sisend.config(font=("Arial", 20))
+    salv = Button(uus, text="Salvesta", command= lambda: salvesta(sisend.get()))
+    salv.grid(row=2, column=1)
+    salv.config(font=("Arial", 20))
+
 #Peaakna loomine
 
 m = Tk()
 laius = m.winfo_screenwidth()
 korgus = m.winfo_screenheight()
-m.geometry("%dx%d" % (laius, korgus))
 m.title("Keelutsooni ennustaja")
+m.resizable(True, True)
 
 #Nupu 'ennusta' loomine
 
@@ -53,12 +92,19 @@ start = Button(m, text='Ennusta', width=25, height=5, command=ennusta)
 start.grid(row=6, column=0)
 start.config(bg='red')
 
-#Menüüd peaakna päisesse
+#Hoiatuse loomine
+
+hoiatus = Label(m, text="Täida kõik väljad numbritega!")
+hoiatus.config(bg='orange', font=("Arial", 25))
+hoiatus.grid(row = 0, columnspan = 5)
+
+#Menüüd ja funktsioonid peaakna päisesse
 
 menuu = Menu(m)
 m.config(menu=menuu)
 filemenu = Menu(menuu)
-menuu.add_cascade(label="Salvesta", menu=filemenu)
+menuu.add_cascade(label="Tühjenda kõik väljad", command=tuhjenda)
+menuu.add_command(label="Salvesta", command=salvestusaken)
 helpmenu = Menu(menuu)
 menuu.add_cascade(label="Abi", menu=helpmenu)
 helpmenu.add_command(label='Kuidas see töötab?', command=Toplevel)
@@ -69,39 +115,23 @@ menuu.add_cascade(label="Väljumine", menu=exitmenu)
 exitmenu.add_command(label="Salvesta ja välju", command=m.quit)
 exitmenu.add_command(label='Välju salvestamata', command=m.quit)
 
-#Hiljem viin järgnevad plokid tsüklisse
 
-Label(m, text='nelements').grid(row=0, column=0, columnspan=2, rowspan=2)
-Label(m, text='volume').grid(row=0, column=3, columnspan=2, rowspan=2)
-Label(m, text='density').grid(row=0, column=6, columnspan=2, rowspan=2)
-Label(m, text='density atomic').grid(row=0, column=9, columnspan=2, rowspan=2)
-Label(m, text='energy per atom').grid(row=0, column=12, columnspan=2, rowspan=2)
-Label(m, text='formation energy per atom').grid(row=3, column=0, columnspan=3, rowspan=2)
-Label(m, text='energy above hull').grid(row=3, column=3, columnspan=2, rowspan=2)
-Label(m, text='efermi').grid(row=3, column=5, columnspan=2, rowspan=2)
-Label(m, text='total magnetization').grid(row=3, column=7, columnspan=4, rowspan=2)
-Label(m, text='universal anisotropy').grid(row=3, column=11, columnspan=4, rowspan=2)
 
-e1 = Entry(m)
-e2 = Entry(m)
-e3 = Entry(m)
-e4 = Entry(m)
-e5 = Entry(m)
-e6 = Entry(m)
-e7 = Entry(m)
-e8 = Entry(m)
-e9 = Entry(m)
-e10 = Entry(m)
+sildid = ["Elementide arv", "Ruumala", "Tihedus", "Atomaarne tihedus", "Energia aatomi kohta", "Tekkimisenergia \n aatomi kohta",
+          'energy above hull', 'efermi', 'total magnetization', 'Universaalne \n anisotroopia']
 
-e1.grid(row=2, column=0)
-e2.grid(row=2,column=3)
-e3.grid(row=2, column=6)
-e4.grid(row=2, column=9)
-e5.grid(row=2, column=12)
-e6.grid(row=5,column=0)
-e7.grid(row=5,column=3)
-e8.grid(row=5,column=6)
-e9.grid(row=5,column=9)
-e10.grid(row=5,column=12)
+sisendid = []
+rida = 2
+veerg = 0
+
+for silt in sildid:
+    Label(m, text = silt).grid(row = rida, column = veerg)
+    s = Entry(m)
+    s.grid(row = rida+1, column = veerg)
+    sisendid.append(s)
+    veerg = veerg + 1
+    if veerg == 5:
+        veerg = 0
+        rida += 2
 
 m.mainloop()
